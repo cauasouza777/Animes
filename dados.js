fbacc42d2549e6a149147027b55c76647e6ba99f
cauasouza777 <cauasouza539@gmail.com>
2024-09-07 00:58:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados =[
+    {
+    titulo: "Satoru Gojo",
+    descricao : "Satoru Gojo é um personagem carismático e poderoso da série de anime e mangá Jujutsu Kaisen. Conhecido como o feiticeiro mais forte, Gojo possui habilidades sobrenaturais excepcionais, como a Manipulação de Energia Amaldiçoada e os Seis Olhos, que lhe conferem uma visão e controle sobre a energia amaldiçoada sem igual.",
+    link : "https://jujutsu-kaisen.fandom.com/wiki/Satoru_Gojo",
+    tags: "feitiçeiro"
+    },
+    {
+     titulo: "Obito Uchiha",
+     descricao: "Obito Uchiha é um personagem complexo do anime e mangá Naruto. Originalmente um membro da respeitada família Uchiha e aliado dos heróis, Obito se torna um antagonista significativo após um trágico incidente e a manipulação de Madara Uchiha. Ele possui o Sharingan e o Rinnegan, habilidades poderosas que fazem dele um dos ninjas mais formidáveis.",
+     link: "https://naruto.fandom.com/wiki/Obito_Uchiha",
+     tags: "ninja"
+     },      
+     {
+
+    titulo: "Sanemi Shinazugawa ",
+    descricao: "Sanemi Shinazugawa é um dos Caçadores de Demônios no anime e mangá Demon Slayer Kimetsu no Yaiba. Como Hashira do Vento, ele é conhecido por sua personalidade intensa e habilidades de combate com a Respiração do Vento. Sanemi possui uma força e agilidade impressionantes, que o tornam um dos principais guerreiros na luta contra os demônios.",
+    link: "https://kimetsu-no-yaiba.fandom.com/wiki/Sanemi_Shinazugawa",
+    tags: "caçador"
+    },
+];
